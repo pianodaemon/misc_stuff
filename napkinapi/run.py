76ad5a066fef77ce72d2f640404a02ff1614a0ee
Edward Nygma <edward@nygma.com>
2018-06-04 17:53:49 -0400

    persistance: newer model package
    Upon this package shall be stored the model required
    by any transfer specification.
    
    Signed-off-by: Edward Nygma <edward@nygma.com>

--- napkinapi/run.py
+++ napkinapi/run.py
@@ -7,15 +7,8 @@ api = Api(app)
 
 from flask_jwt import JWT
 from werkzeug.security import safe_str_cmp
+from persistence.models.users import User
 
-class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __str__(self):
-        return "User(id='%s')" % self.id
 
 users = [
     User(1, 'user1', 'abcxyz'),
