c76321b3cfe3ba14b20107037540c9baa2bded16
Edwin Plauchu <pianodaemon@gmail.com>
2024-05-30 07:26:22 -0600

    Update ec.py

--- eventcrawler/ec.py
+++ eventcrawler/ec.py
@@ -2,6 +2,8 @@
 
 from bcc import BPF
 import json
+import datetime
+import time
 
 # BPF program
 bpf_program = """
@@ -12,6 +14,7 @@ bpf_program = """
 BPF_PERF_OUTPUT(bpf_buffer_00);
 
 struct execve_data_t {
+    u64 ts;
     u64 uid;
     u32 pid;
     u32 ppid;
@@ -31,6 +34,7 @@ TRACEPOINT_PROBE(syscalls, sys_enter_execve) {
     data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
     bpf_probe_read_user_str(&data.filename, sizeof(data.filename), args->filename);
+    data.ts = bpf_ktime_get_ns();
     bpf_buffer_00.perf_submit(args, &data, sizeof(data));
 
     return 0;
@@ -44,6 +48,14 @@ bpf.detach_tracepoint("syscalls:sys_enter_execve")
 # Attach the BPF program to the sys_enter_execve tracepoint
 bpf.attach_tracepoint(tp="syscalls:sys_enter_execve", fn_name="tracepoint__syscalls__sys_enter_execve")
 
+time_nanosec = time.time_ns()
+
+def nanoseconds_to_datetime(nanoseconds):
+    seconds = nanoseconds / 1e9
+    dt = datetime.datetime.utcfromtimestamp(seconds)
+    readable_time = dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # Trim to milliseconds
+    return readable_time
+
 def print_event(cpu, data, size):
     event = bpf["bpf_buffer_00"].event(data)
     dj = json.dumps({
@@ -52,6 +64,8 @@ def print_event(cpu, data, size):
        "uid": event.uid,
        "comm": event.comm.decode('utf-8', 'replace'),
        "filename": event.filename.decode('utf-8', 'replace'),
+       "utcdatetime": nanoseconds_to_datetime(time_nanosec - event.ts),
+       "monots": event.ts,
     })
     print(dj)
 
