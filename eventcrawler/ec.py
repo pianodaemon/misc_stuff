199144d398a7bbf7d227a07cbb03f2e1b4b92d66
Edwin Plauchu <pianodaemon@gmail.com>
2024-05-30 16:41:49 -0600

    Update ec.py

--- eventcrawler/ec.py
+++ eventcrawler/ec.py
@@ -11,7 +11,7 @@ bpf_program = """
 #include <linux/sched.h>
 #include <linux/limits.h>
 
-BPF_PERF_OUTPUT(bpf_buffer_00);
+BPF_PERF_OUTPUT(PERF_BUFFER_00);
 
 struct execve_data_t {
     u64 ts;
@@ -35,47 +35,68 @@ TRACEPOINT_PROBE(syscalls, sys_enter_execve) {
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
     bpf_probe_read_user_str(&data.filename, sizeof(data.filename), args->filename);
     data.ts = bpf_ktime_get_ns();
-    bpf_buffer_00.perf_submit(args, &data, sizeof(data));
+    PERF_BUFFER_00.perf_submit(args, &data, sizeof(data));
 
     return 0;
 }
 """
 
-# Load BPF program
-bpf = BPF(text=bpf_program)
-bpf.detach_tracepoint("syscalls:sys_enter_execve")
+class EventCrawler(object):
 
-# Attach the BPF program to the sys_enter_execve tracepoint
-bpf.attach_tracepoint(tp="syscalls:sys_enter_execve", fn_name="tracepoint__syscalls__sys_enter_execve")
+    __BPF_PERF = "PERF_BUFFER_00"
+    __TP_HOOK = "sys_enter_execve"
+    __TP_FN = f"tracepoint__syscalls__{__TP_HOOK}"
+    __TP_SYSCALL = f"syscalls:{__TP_HOOK}"
 
-time_nanosec = time.time_ns()
+    def __init__(self, prog_src):
+        self._prog_src = prog_src
+        self._time_increment = 0
 
-def nanoseconds_to_datetime(nanoseconds):
-    seconds = nanoseconds / 1e9
-    dt = datetime.datetime.utcfromtimestamp(seconds)
-    readable_time = dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # Trim to milliseconds
-    return readable_time
+    @classmethod
+    def spinup(cls, prog_src):
+        print("Tracing execve... Press Ctrl+C to stop.")
+        while True:
+            try:
+                crawler = cls(bpf_program)
+                crawler._engage()
+            except KeyboardInterrupt:
+                exit(0)
 
-def print_event(cpu, data, size):
-    event = bpf["bpf_buffer_00"].event(data)
-    dj = json.dumps({
-       "ppid": event.ppid,
-       "pid": event.pid,
-       "uid": event.uid,
-       "comm": event.comm.decode('utf-8', 'replace'),
-       "filename": event.filename.decode('utf-8', 'replace'),
-       "utcdatetime": nanoseconds_to_datetime(time_nanosec - event.ts),
-       "monots": event.ts,
-    })
-    print(dj)
+    def _setup_bpf(self):
+        self._bpf = BPF(text=self._prog_src)
+        self._bpf.detach_tracepoint(self.__TP_SYSCALL)
+        self._bpf.attach_tracepoint(tp=self.__TP_SYSCALL, fn_name=self.__TP_FN)
+        self._bpf[self.__BPF_PERF].open_perf_buffer(self._print_event)
 
-bpf["bpf_buffer_00"].open_perf_buffer(print_event)
+    def _engage(self):
+        '''Blocks as long as it reads buffer'''
+        self._setup_bpf()
+        self._time_nanosec = time.time_ns()
+        while True:
+            self._bpf.perf_buffer_poll()
 
-print("Tracing execve... Press Ctrl+C to stop.")
+    @staticmethod
+    def _nanoseconds_to_datetime(nanoseconds):
+        seconds = nanoseconds / 1e9
+        dt = datetime.datetime.utcfromtimestamp(seconds)
+        return dt.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]  # Trim to milliseconds
 
-# Poll the perf buffer to process events
-while True:
-    try:
-        bpf.perf_buffer_poll()
-    except KeyboardInterrupt:
-        exit()
+    def _print_event(self, cpu, data, size):
+        event = self._bpf[self.__BPF_PERF].event(data)
+        if self._time_increment > 0:
+            self._time_increment = event.ts - self._time_increment
+        self._time_nanosec += self._time_increment
+        dj = json.dumps({
+           "ppid": event.ppid,
+           "pid": event.pid,
+           "uid": event.uid,
+           "comm": event.comm.decode('utf-8', 'replace'),
+           "filename": event.filename.decode('utf-8', 'replace'),
+           "utcdatetime": self._nanoseconds_to_datetime(self._time_nanosec),
+           "monots": event.ts,
+        })
+        self._time_increment = event.ts
+        print(dj)
+
+if __name__ == "__main__":
+    EventCrawler.spinup(bpf_program)
